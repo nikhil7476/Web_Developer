da49a2cce2b3a49635dc014274c869534e0af77e
nikhil7476 <nikhil9027917476@gmail.com>
2025-12-05 03:29:11 +0530

    modified:   nikhil-mishra/src/lib/mongodb.js

--- nikhil-mishra/src/lib/mongodb.js
+++ nikhil-mishra/src/lib/mongodb.js
@@ -2,21 +2,30 @@ import mongoose from "mongoose";
 
 const MONGODB_URI = process.env.MONGODB_URI;
 
-if (!MONGODB_URI) throw new Error("Missing MONGODB_URI");
+if (!MONGODB_URI) {
+  throw new Error("Please define the MONGODB_URI in your .env.local file");
+}
 
-let cached = global.mongoose || { conn: null, promise: null };
+let cached = global.mongoose;
 
-global.mongoose = cached;
+if (!cached) {
+  cached = global.mongoose = { conn: null, promise: null };
+}
 
-export default async function connectDB() {
+async function connectDB() {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
     cached.promise = mongoose
-      .connect(MONGODB_URI, { bufferCommands: false })
+      .connect(MONGODB_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      })
       .then((mongoose) => mongoose);
   }
 
   cached.conn = await cached.promise;
   return cached.conn;
 }
+
+export default connectDB;
