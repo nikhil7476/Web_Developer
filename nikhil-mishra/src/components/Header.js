0cbf35894d709b1764b2a8bd74e4738fb5cf3f1c
nikhil7476 <nikhil9027917476@gmail.com>
2025-12-05 19:36:52 +0530

    modified:   nikhil-mishra/src/components/Header.js

--- nikhil-mishra/src/components/Header.js
+++ nikhil-mishra/src/components/Header.js
@@ -1,9 +1,83 @@
-import React from 'react'
+import { Col, Container, Nav, Navbar, Offcanvas, Row } from "react-bootstrap";
+import Image from "next/image";
 
 function Header() {
+  const expand = "lg";
+
   return (
-    <div>Header</div>
-  )
+    <>
+      <section className="main-header fixed-top bg-white shadow">
+        <Container>
+          <Row>
+            <Col>
+              <Navbar expand={expand}>
+                <Container fluid>
+                  <Navbar.Brand href="/" title="Nikhil Mishra">
+                    <Image
+                      src="/nikhil-logo.webp"
+                      alt="Nikhil Mishra"
+                      title="Nikhil Mishra"
+                      width={50}
+                      height={50}
+                      className="rounded-circle shadow"
+                    />{" "}
+                    Nikhil Mishra
+                  </Navbar.Brand>
+                  <Navbar.Toggle
+                    aria-controls={`offcanvasNavbar-expand-${expand}`}
+                  />
+                  <Navbar.Offcanvas
+                    id={`offcanvasNavbar-expand-${expand}`}
+                    aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
+                    placement="end"
+                  >
+                    <Offcanvas.Header closeButton>
+                      <Offcanvas.Title
+                        id={`offcanvasNavbarLabel-expand-${expand}`}
+                      >
+                        <Image
+                          src="/nikhil-logo.webp"
+                          alt="Nikhil Mishra"
+                          title="Nikhil Mishra"
+                          width={50}
+                          height={50}
+                          className="rounded-circle shadow"
+                        />{" "}
+                        Nikhil Mishra
+                      </Offcanvas.Title>
+                    </Offcanvas.Header>
+
+                    <Offcanvas.Body>
+                      <Nav className="justify-content-end flex-grow-1 pe-3">
+                        <Nav.Link href="/" title="Home">
+                          Home
+                        </Nav.Link>
+                        <Nav.Link href="/about-us" title="About Us">
+                          About Us
+                        </Nav.Link>
+                        <Nav.Link href="/services" title="Services">
+                          Services
+                        </Nav.Link>
+                        <Nav.Link href="/portfolio" title="Portfolio">
+                          Portfolio
+                        </Nav.Link>
+                        <Nav.Link href="/blog" title="Articles">
+                          Articles
+                        </Nav.Link>
+                        <Nav.Link href="/contact-us" title="Contact Us">
+                          Contact Us
+                        </Nav.Link>
+                      </Nav>
+                    </Offcanvas.Body>
+                  </Navbar.Offcanvas>
+                </Container>
+              </Navbar>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </>
+  );
 }
 
-export default Header
+export default Header;
